2473fc905dcb6ad3453e7794abbb70e50b330e7b
Harrison Brock <harrison@harrisonbrock.com>
2018-09-24 18:10:10 -0400

    ADDED Mux, and created two new routes from home and contact

--- main.go
+++ main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func handlerFunc(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "<h1>Welcome to my awesome site!</h1>")
 }
+
+// home
+func home(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(writer, "<h1>Welcome to my awesome site!</h1>")
+
+}
+
+func conact(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(writer, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3030", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", conact)
+	http.ListenAndServe(":3030", router)
 }
